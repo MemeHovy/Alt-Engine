b6f10f9d955d167095b9938197638c220c00a0a3
Zhenua Kovalev <111678905+Fearester@users.noreply.github.com>
2023-01-07 20:55:38 +0300

    Update FlxSplash.hx

--- source/flixel/system/FlxSplash.hx
+++ source/flixel/system/FlxSplash.hx
@@ -1,205 +1,66 @@
 package flixel.system;
-
-import flash.display.Graphics;
-import flash.display.Sprite;
+ 
+import flixel.FlxSprite;
+import flash.display.Bitmap;
+import flash.display.BitmapData;
+import flash.display.BlendMode;
+import flash.display.FlxSprite;
 import flash.Lib;
-import flash.text.TextField;
-import flash.text.TextFormat;
-import flash.text.TextFormatAlign;
 import flixel.FlxG;
-import flixel.FlxState;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
-import flixel.util.FlxColor;
-import flixel.util.FlxTimer;
-
-class FlxSplash extends FlxState
+import flixel.system.FlxSound;
+import flixel.system.FlxBasePreloader;
+ 
+class Preloader extends FlxBasePreloader
 {
-	public static var nextState:Class<FlxState>;
-
-	/**
-	 * @since 4.8.0
-	 */
-	public static var muted:Bool = #if html5 true #else false #end;
-
-	var _sprite:Sprite;
-	var _gfx:Graphics;
-	var _text:TextField;
-
-	var _times:Array<Float>;
-	var _colors:Array<Int>;
-	var _functions:Array<Void->Void>;
-	var _curPart:Int = 0;
-	var _cachedBgColor:FlxColor;
-	var _cachedTimestep:Bool;
-	var _cachedAutoPause:Bool;
-
-	override public function create():Void
-	{
-		_cachedBgColor = FlxG.cameras.bgColor;
-		FlxG.cameras.bgColor = FlxColor.BLACK;
-
-		// This is required for sound and animation to synch up properly
-		_cachedTimestep = FlxG.fixedTimestep;
-		FlxG.fixedTimestep = false;
-
-		_cachedAutoPause = FlxG.autoPause;
-		FlxG.autoPause = false;
-
-		#if FLX_KEYBOARD
-		FlxG.keys.enabled = false;
-		#end
-
-		_times = [0.039, 0.165, 0.302, 0.488, 0.631];
-		_colors = [0x00b922, 0xffc132, 0xf5274e, 0x3641ff, 0x04cdfb];
-		_functions = [drawGreen, drawYellow, drawRed, drawBlue, drawLightBlue];
-
-		for (time in _times)
-		{
-			new FlxTimer().start(time, timerCallback);
-		}
-
-		var stageWidth:Int = Lib.current.stage.stageWidth;
-		var stageHeight:Int = Lib.current.stage.stageHeight;
-
-		_sprite = new Sprite();
-		FlxG.stage.addChild(_sprite);
-		_gfx = _sprite.graphics;
-
-		_text = new TextField();
-		_text.selectable = false;
-		_text.embedFonts = true;
-		var dtf = new TextFormat(FlxAssets.FONT_DEFAULT, 16, 0xffffff);
-		dtf.align = TextFormatAlign.CENTER;
-		_text.defaultTextFormat = dtf;
-		_text.text = "Friday Night Funkin' Alt Engine";
-		FlxG.stage.addChild(_text);
-
-		onResize(stageWidth, stageHeight);
-
-		#if FLX_SOUND_SYSTEM
-		if (!muted)
-		{
-			FlxG.sound.load(Paths.music("start_screen")).play();
-		}
-		#end
+    public function new(MinDisplayTime:Float= 5, ?AllowedURLs:Array<String>) 
+    {
+        super(MinDisplayTime, AllowedURLs);
+    }
+     
+    var logo:FlxSprite;
+    var LogoBG:FlxSprite;
+    var LogoText:FlxSprite;
+    var StartSong:FlxSound;
+     
+    override function create():Void 
+    {
+        this._width = Lib.current.stage.stageWidth;
+        this._height = Lib.current.stage.stageHeight;
+         
+        var ratio:Float = this._width / 2560; //This allows us to scale assets depending on the size of the screen.
+         
+        LogoBG = new FlxSprite(0,0).makeGraphic(FlxG.width, FlxG.height, 0xFFffffff);
+        LogoBG.screenCenter(X);
+        addChild(LogoBG);
+        logo = new FlxSprite(-1900,0);
+        logo.loadGraphic(Paths.image('Haxe Logo'));
+        logo.scrollFactor.set();
+        addChild(logo);
+        LogoText = new FlxSprite(1700,0);
+        LogoText.loadGraphic(Paths.image('Haxe Text'));
+        LogoText.scrollFactor.set();
+        addChild(LogoText);
+        StartSong = new FlxSound().loadEmbedded(Paths.sound('start'));
+		FlxG.sound.list.add(StartSong);
+        super.create();
+    }
+     
+    override function update(elapsed:Float):Void 
+    {
+    FlxTween.tween(logo, {x: 400}, 5, {
+	ease: FlxEase.backInOut,
+	onComplete: function(twn:FlxTween) {
+	FlxTween.tween(logo, {x: -1900}, 5, {ease: FlxEase.backInOut});
 	}
-
-	override public function destroy():Void
-	{
-		_sprite = null;
-		_gfx = null;
-		_text = null;
-		_times = null;
-		_colors = null;
-		_functions = null;
-		super.destroy();
+	});
+	FlxTween.tween(LogoText, {x: 700}, 5, {
+	ease: FlxEase.backInOut,
+	onComplete: function(twn:FlxTween) {
+	FlxTween.tween(LogoText, {x: 1700}, 5, {ease: FlxEase.backInOut});
 	}
-
-	override public function onResize(Width:Int, Height:Int):Void
-	{
-		super.onResize(Width, Height);
-
-		_sprite.x = (Width / 2);
-		_sprite.y = (Height / 2) - 20 * FlxG.game.scaleY;
-
-		_text.width = Width / FlxG.game.scaleX;
-		_text.x = 0;
-		_text.y = _sprite.y + 80 * FlxG.game.scaleY;
-
-		_sprite.scaleX = _text.scaleX = FlxG.game.scaleX;
-		_sprite.scaleY = _text.scaleY = FlxG.game.scaleY;
-	}
-
-	function timerCallback(Timer:FlxTimer):Void
-	{
-		_functions[_curPart]();
-		_text.textColor = _colors[_curPart];
-		_text.text = "Friday Night Funkin' Alt Engine";
-		_curPart++;
-
-		if (_curPart == 5)
-		{
-			// Make the logo a tad bit longer, so our users fully appreciate our hard work :D
-			FlxTween.tween(_sprite, {alpha: 0}, 3.0, {ease: FlxEase.quadOut, onComplete: onComplete});
-			FlxTween.tween(_text, {alpha: 0}, 3.0, {ease: FlxEase.quadOut});
-		}
-	}
-
-	function drawGreen():Void
-	{
-		_gfx.beginFill(0x00b922);
-		_gfx.moveTo(0, -37);
-		_gfx.lineTo(1, -37);
-		_gfx.lineTo(37, 0);
-		_gfx.lineTo(37, 1);
-		_gfx.lineTo(1, 37);
-		_gfx.lineTo(0, 37);
-		_gfx.lineTo(-37, 1);
-		_gfx.lineTo(-37, 0);
-		_gfx.lineTo(0, -37);
-		_gfx.endFill();
-	}
-
-	function drawYellow():Void
-	{
-		_gfx.beginFill(0xffc132);
-		_gfx.moveTo(-50, -50);
-		_gfx.lineTo(-25, -50);
-		_gfx.lineTo(0, -37);
-		_gfx.lineTo(-37, 0);
-		_gfx.lineTo(-50, -25);
-		_gfx.lineTo(-50, -50);
-		_gfx.endFill();
-	}
-
-	function drawRed():Void
-	{
-		_gfx.beginFill(0xf5274e);
-		_gfx.moveTo(50, -50);
-		_gfx.lineTo(25, -50);
-		_gfx.lineTo(1, -37);
-		_gfx.lineTo(37, 0);
-		_gfx.lineTo(50, -25);
-		_gfx.lineTo(50, -50);
-		_gfx.endFill();
-	}
-
-	function drawBlue():Void
-	{
-		_gfx.beginFill(0x3641ff);
-		_gfx.moveTo(-50, 50);
-		_gfx.lineTo(-25, 50);
-		_gfx.lineTo(0, 37);
-		_gfx.lineTo(-37, 1);
-		_gfx.lineTo(-50, 25);
-		_gfx.lineTo(-50, 50);
-		_gfx.endFill();
-	}
-
-	function drawLightBlue():Void
-	{
-		_gfx.beginFill(0x04cdfb);
-		_gfx.moveTo(50, 50);
-		_gfx.lineTo(25, 50);
-		_gfx.lineTo(1, 37);
-		_gfx.lineTo(37, 1);
-		_gfx.lineTo(50, 25);
-		_gfx.lineTo(50, 50);
-		_gfx.endFill();
-	}
-
-	function onComplete(Tween:FlxTween):Void
-	{
-		FlxG.cameras.bgColor = _cachedBgColor;
-		FlxG.fixedTimestep = _cachedTimestep;
-		FlxG.autoPause = _cachedAutoPause;
-		#if FLX_KEYBOARD
-		FlxG.keys.enabled = true;
-		#end
-		FlxG.stage.removeChild(_sprite);
-		FlxG.stage.removeChild(_text);
-		FlxG.switchState(Type.createInstance(nextState, []));
-		FlxG.game._gameJustStarted = true;
-	}
+	});
+        super.update(elapsed);
+    }
 }
