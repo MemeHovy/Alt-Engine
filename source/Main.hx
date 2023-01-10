faea8d7f798770e0f9ebfbd2646a6d63a018c242
Rom4chek <97591537+Rom4chek@users.noreply.github.com>
2023-01-10 12:47:38 +0600

    nvm, ill do it later ig

--- source/Main.hx
+++ source/Main.hx
@@ -136,9 +136,8 @@ class Main extends Sprite
 
 		#if html5
 		FlxG.mouse.visible = false;
-		#end
-			
 		FlxG.autoPause = false;
+		#end
 			
 		#if cpp
 		cpp.NativeGc.enable(true);
@@ -188,79 +187,14 @@ class Main extends Sprite
 				}
 			}
 		}
-	
-	var newgame:FlxGame;
-	var oldVol:Float = 1.0;
-	var newVol:Float = 0.3;
 
-	public static var focused:Bool = true;
-
-	// thx for ur code ari
-	function onWindowFocusOut()
-		{
-			focused = false;
-	
-			// Lower global volume when unfocused
-			if (Type.getClass(FlxG.state) != PlayState) // imagine stealing my code smh
-			{
-				oldVol = FlxG.sound.volume;
-				if (oldVol > 0.3)
-				{
-					newVol = 0.3;
-				}
-				else
-				{
-					if (oldVol > 0.1)
-					{
-						newVol = 0.1;
-					}
-					else
-					{
-						newVol = 0;
-					}
-				}
-	
-				//trace("Game unfocused");
-	
-				if (focusMusicTween != null)
-					focusMusicTween.cancel();
-				focusMusicTween = FlxTween.tween(FlxG.sound, {volume: newVol}, 0.5);
-	
-				// Conserve power by lowering draw framerate when unfocuced
-				FlxG.drawFramerate = 60;
-			}
-		}
-	
-		function onWindowFocusIn()
-		{
-			new FlxTimer().start(0.2, function(tmr:FlxTimer)
-			{
-				focused = true;
-			});
-	
-			// Lower global volume when unfocused
-			if (Type.getClass(FlxG.state) != PlayState)
-			{
-				//trace("Game focused");
-	
-				// Normal global volume when focused
-				if (focusMusicTween != null)
-					focusMusicTween.cancel();
-	
-				focusMusicTween = FlxTween.tween(FlxG.sound, {volume: oldVol}, 0.5);
-	
-				// Bring framerate back when focused
-				FlxG.drawFramerate = 60;
-			}
-		}
-
-		public static function gc() {
+	public static function gc() {
 			#if cpp
 			Gc.run(true);
 			#else
 			openfl.system.System.gc();
 			#end
-		}
+	}
 
 	// Code was entirely made by sqirra-rng for their fnf engine named "Izzy Engine", big props to them!!!
 	// very cool person for real they don't get enough credit for their work
