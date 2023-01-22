8e3c31608a30996bd7daa056357c5786fe928b83
Zhenua Kovalev <111678905+Fearester@users.noreply.github.com>
2023-01-22 14:56:16 +0300

    Update and rename GPUSubState.hx to LanguageSubState.hx

---
+++ source/options/LanguageSubState.hx
@@ -0,0 +1,48 @@
+package options;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flixel.ui.FlxBar;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+import openfl.Lib;
+
+using StringTools;
+
+class LanguageSubState extends BaseOptionsMenu
+{
+     public function new()
+	{
+		title = 'Language Select';
+		rpcTitle = 'Select Language menu'; //for Discord Rich Presence
+
+		var option:Option = new Option('Language',
+			"Select Language",
+			'Language',
+			'string',
+			"English","Russian");
+		addOption(option);
+		super();
+	}
+}
